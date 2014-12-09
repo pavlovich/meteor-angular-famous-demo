5616c220750686bcb4dd1b981bb6069c39d6eddd
Peter Pavlovich <pavlovich@gmail.com>
2014-12-09 00:55:20 -0500

    fixed layout issue by adding required CSS

--- client/main.js
+++ client/main.js
@@ -1,14 +1,15 @@
-
-
-
+angular.module('taskMaster', ['angular-meteor', 'famous.angular'])
+    .controller('mainController', ['$scope', function($scope){
+        $scope.myGridLayoutOptions = {
+            dimensions: [1,3]
+        };
+        $scope.items = [
+            {name: 'one', height: 100, color: 'yellow'},
+            {name: 'two', height: 600, color: 'green'},
+            {name: 'three', height: 200, color: 'blue'}
+        ];
+    }]);
 
 Meteor.startup(function () {
-    angular.module('taskMaster', ['angular-meteor', 'famous.angular'])
-        .controller('mainController', ['$scope', function($scope){
-            $scope.myGridLayoutOptions = {
-                dimensions: [1,3]
-            };
-        }]);
-
     angular.bootstrap(document, ['taskMaster']);
 });
